10e31bf9e919f29c66547893201800b202b9b225
Abasifreke Isaac <79318104+Abas-dev@users.noreply.github.com>
2024-07-07 23:23:12 +0100

    build

--- build.sh
+++ build.sh
@@ -5,8 +5,7 @@ echo "  BUILD START"
 source venv/bin/activate
 
 pip3 install -r requirements.txt
-python3.12 manage.py makemigrations
-python3.12 manage.py migrate
+
 python3.12 manage.py collectstatic --noinput 
 
 echo "  BUILD END"
