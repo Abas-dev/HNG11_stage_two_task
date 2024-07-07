584222ffe5dc5fa65d4b472ff213b417d2236224
Abasifreke Isaac <79318104+Abas-dev@users.noreply.github.com>
2024-07-07 23:09:35 +0100

    build

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
-set -o errexit
+#!/bin/bash 
 
-pip install -r requiremennts.txt
-
-python manage.py collectstatic --no-input
+pip install -r requirements.txt
+python manage.py makemigrations
+python manage.py migrate
+python manage.py collectstatic --noinput 
