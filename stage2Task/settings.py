cb42b9b3e2dd10e86ea3bdce3fd6c3b3afbd57eb
Abasifreke Isaac <79318104+Abas-dev@users.noreply.github.com>
2024-07-07 23:41:54 +0100

    build

--- stage2Task/settings.py
+++ stage2Task/settings.py
@@ -39,9 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
     'accounts.apps.AccountsConfig',
-
-    "accounts"
 
     'rest_framework',
     'rest_framework_simplejwt'
